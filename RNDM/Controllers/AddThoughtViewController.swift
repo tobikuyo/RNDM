7396dc5686f255df48f0c5ec200944b13bd13c61
Tobi Tselios <tobi.tselios@gmail.com>
2020-06-27 21:40:46 +0100

    Creating posts and sending them to Firestore

--- RNDM/Controllers/AddThoughtViewController.swift
+++ RNDM/Controllers/AddThoughtViewController.swift
@@ -7,18 +7,30 @@
 //
 
 import UIKit
+import Firebase
 
 class AddThoughtViewController: UIViewController {
+
+    // MARK: - IBOutlets
 
     @IBOutlet var categorySegment: UISegmentedControl!
     @IBOutlet var usernameTextField: UITextField!
     @IBOutlet var thoughtTextView: UITextView!
     @IBOutlet var postButton: UIButton!
 
+    // MARK: - Properties
+
+    private let database = Firestore.firestore()
+    private var selectedCategory = ThoughtCategory.funny.rawValue
+
+    // MARK: - View Lifecycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
         updateViews()
     }
+
+    // MARK: - Methods
 
     func updateViews() {
         postButton.layer.cornerRadius = 5
@@ -28,10 +40,38 @@ class AddThoughtViewController: UIViewController {
         thoughtTextView.delegate = self
     }
 
+    // MARK: - IBActions
+
     @IBAction func categoryChanged(_ sender: Any) {
+        switch categorySegment.selectedSegmentIndex {
+        case 0:
+            selectedCategory = ThoughtCategory.funny.rawValue
+        case 1:
+            selectedCategory = ThoughtCategory.serious.rawValue
+        default:
+            selectedCategory = ThoughtCategory.crazy.rawValue
+        }
     }
 
     @IBAction func postButtonTapped(_ sender: Any) {
+        guard
+            let thoughtText = thoughtTextView.text,
+            let username = usernameTextField.text else { return }
+
+        database.collection(K.thoughts).addDocument(data: [
+            K.category: selectedCategory,
+            K.numComments: 0,
+            K.numLikes: 0,
+            K.thoughtText: thoughtText,
+            K.timestamp: FieldValue.serverTimestamp(),
+            K.username: username
+        ]) { error in
+            if let error = error {
+                print("Error adding document: \(error.localizedDescription)")
+            } else {
+                self.navigationController?.popViewController(animated: true)
+            }
+        }
     }
 }
 
