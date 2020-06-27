90b88be227ecfb03598287e281105d10d4cb2140
Tobi Tselios <tobi.tselios@gmail.com>
2020-06-27 19:02:02 +0100

    Initial commit

---
+++ RNDM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RNDM
+//
+//  Created by Tobi Kuyoro on 27/06/2020.
+//  Copyright © 2020 Tobi Kuyoro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
