0b991d0d74c2fa57e92ae01f5ce4e52259206154
Tobi Tselios <tobi.tselios@gmail.com>
2020-06-27 21:39:11 +0100

    Initial Constants file setup

---
+++ RNDM/Helpers/Constants.swift
@@ -0,0 +1,19 @@
+//
+//  Constants.swift
+//  RNDM
+//
+//  Created by Tobi Kuyoro on 27/06/2020.
+//  Copyright © 2020 Tobi Kuyoro. All rights reserved.
+//
+
+import Foundation
+
+struct K {
+    static let thoughts     = "thoughts"
+    static let category     = "category"
+    static let numComments  = "numComments"
+    static let numLikes     = "numLikes"
+    static let thoughtText  = "thoughtText"
+    static let timestamp    = "timestamp"
+    static let username     = "username"
+}
