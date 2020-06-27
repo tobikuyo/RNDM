f4b136cffb2f4f5c40d75a9477bd07069e0168a8
Tobi Tselios <tobi.tselios@gmail.com>
2020-06-28 00:48:49 +0100

    ThoughtTableViewCell setup

---
+++ RNDM/Views/ThoughtTableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  ThoughtTableViewCell.swift
+//  RNDM
+//
+//  Created by Tobi Kuyoro on 28/06/2020.
+//  Copyright © 2020 Tobi Kuyoro. All rights reserved.
+//
+
+import UIKit
+
+class ThoughtTableViewCell: UITableViewCell {
+
+    @IBOutlet var usernameLabel: UILabel!
+    @IBOutlet var timestampLabel: UILabel!
+    @IBOutlet var thoughtTextLabel: UILabel!
+    @IBOutlet var likesImage: UIImageView!
+    @IBOutlet var likesCountLabel: UILabel!
+
+    func configureCell(thought: Thought) {
+
+    }
+}
