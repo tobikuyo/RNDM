f3f9ba9599ada9b403f5b6c427c3bb82fd0609b2
Tobi Tselios <tobi.tselios@gmail.com>
2020-06-27 21:38:27 +0100

    ThoughtCategory enum for category segmented control

---
+++ RNDM/Helpers/ThoughtCategory.swift
@@ -0,0 +1,16 @@
+//
+//  ThoughtCategory.swift
+//  RNDM
+//
+//  Created by Tobi Kuyoro on 27/06/2020.
+//  Copyright © 2020 Tobi Kuyoro. All rights reserved.
+//
+
+import Foundation
+
+enum ThoughtCategory: String {
+    case serious
+    case funny
+    case crazy
+    case popular
+}
