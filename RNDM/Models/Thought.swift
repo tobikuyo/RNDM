b07d94d4762f7be7721bb2b8770e6924eea856ab
Tobi Tselios <tobi.tselios@gmail.com>
2020-06-28 00:27:12 +0100

    Thought model created

---
+++ RNDM/Models/Thought.swift
@@ -0,0 +1,29 @@
+//
+//  Thought.swift
+//  RNDM
+//
+//  Created by Tobi Kuyoro on 27/06/2020.
+//  Copyright © 2020 Tobi Kuyoro. All rights reserved.
+//
+
+import Foundation
+
+class Thought {
+    private(set) var username: String
+    private(set) var thoughtText: String
+    private(set) var category: String
+    private(set) var numLikes: Int
+    private(set) var numComments: Int
+    private(set) var timestamp: Date
+    private(set) var documentID: String
+
+    init(username: String, thoughtText: String, category: String, numLikes: Int, numComments: Int, timestamp: Date, documentID: String) {
+        self.username = username
+        self.thoughtText = thoughtText
+        self.category = category
+        self.numLikes = numLikes
+        self.numComments = numComments
+        self.timestamp = timestamp
+        self.documentID = documentID
+    }
+}
