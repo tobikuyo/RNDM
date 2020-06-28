61f8fa80b3bb5cd032a142c747351f2dc56a3613
Tobi Tselios <tobi.tselios@gmail.com>
2020-06-28 01:49:51 +0100

    Removed category from Thought model

--- RNDM/Models/Thought.swift
+++ RNDM/Models/Thought.swift
@@ -11,16 +11,14 @@ import Foundation
 class Thought {
     private(set) var username: String
     private(set) var thoughtText: String
-    private(set) var category: String
     private(set) var numLikes: Int
     private(set) var numComments: Int
     private(set) var timestamp: Date
     private(set) var documentID: String
 
-    init(username: String, thoughtText: String, category: String, numLikes: Int, numComments: Int, timestamp: Date, documentID: String) {
+    init(username: String, thoughtText: String, numLikes: Int, numComments: Int, timestamp: Date, documentID: String) {
         self.username = username
         self.thoughtText = thoughtText
-        self.category = category
         self.numLikes = numLikes
         self.numComments = numComments
         self.timestamp = timestamp
